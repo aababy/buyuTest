a6a7e45ab5d601f6e1ed0f92d9d7cc4443472d27
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2017-11-23 14:39:40 +0800

    no message

--- assets/Script/Bezier.js
+++ assets/Script/Bezier.js
@@ -12,7 +12,8 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
-        bez : [cc.Vec2],
+        bez : [],
+        index : 1,
     },
 
     // use this for initialization
@@ -20,25 +21,27 @@ cc.Class({
         this.scheduleOnce(this.runBezier, 2);
         //this.draw();
 
-        var array = [10.0, 2.96, 6.85, 3.23, 9.9, 8.57, 0, 8.3];
+        var array = [
+            [10.0, 2.96, 6.85, 3.23, 9.9, 8.57, 0, 8.3],
+            [0, 7.85, 13.1, 7.02, 2.79, 0.49, 10, 0.58],
+            [0, 1.92, 4.34, 6.45, 7.88, 4.55, 10, 10],
+        ];
+
         var win = cc.winSize;
         var x = 0, y = 0;
-        for (let i = 0; i < array.length; i++) {
-            if (i % 2 == 0) {
-                x = array[i]/10 * win.width - win.width / 2;
-            } else {
-                y = array[i]/10 * win.height - win.height / 2;
-                this.bez.push(cc.v2(x, y));
+        for(let i = 0; i < array.length; i++) {
+            this.bez[i] = new Array();
+            for (let j = 0; j < array[i].length; j++) {
+                if (j % 2 == 0) {
+                    x = array[i][j]/10 * win.width - win.width / 2;
+                } else {
+                    y = array[i][j]/10 * win.height - win.height / 2;
+                    this.bez[i].push(cc.v2(x, y));
+                }
             }
         }
 
-        // var x1 = cc.v2(array[2]/10 * win.width, array[3]/10 * win.height);
-        // var x2 = cc.v2(array[4]/10 * win.width, array[5]/10 * win.height);
-        // var x3 = cc.v2(array[6]/10 * win.width, array[7]/10 * win.height);
-
-        // cc.log(x0 + " " + x1 + " " + x2 + " " + x3);
-        this.node.position = this.bez[0];
-        cc.log(this.bez[0]);   
+        this.index = 2;
     },
 
     generateCoins: function () {
@@ -62,14 +65,11 @@ cc.Class({
 
     //http://www.j--d.com/bezier 工具
     runBezier: function () {
-        // this.node.position = cc.v2.ZERO; //this.bez[0];
-
-        // var bezierParam = [cc.p(0, 166), cc.p(0, 166), cc.p(0, 0)];     
-        // var bezier = cc.bezierBy(4, bezierParam);
+        this.node.position = this.bez[this.index][0];
 
         // var bezierParam = [cc.p(0, cc.winSize.height / 2), cc.p(300, -cc.winSize.height / 2), cc.p(300, 100)];
-        var bezierParam = [this.bez[1], this.bez[2], this.bez[3]];     //抛物线
-        var bezier = cc.bezierTo(4, bezierParam);
+        var bezierParam = [this.bez[this.index][1], this.bez[this.index][2], this.bez[this.index][3]];     //抛物线
+        var bezier = cc.bezierTo(16, bezierParam);
 
         this.node.runAction(bezier);
     },
